330ec29aa95f67789e9d8e51dd829459c5072e28
Jake Champion <jakechampion.jake2@googlemail.com>
2019-07-24 17:59:19 +0100

    fix: make it work with npx

--- index.js
+++ index.js
@@ -11,16 +11,51 @@ require("yargs")
             demandOption: true
         }
     }, async function parseFile(argv) {
-        const execa = require('execa');
         const generatePolyfillURL = require('./src/index.js');
         const path = require('path');
+        const babel = require("@babel/core");
+        const plugin = require('@financial-times/js-features-analyser/src/index.js');
+        const fs = require('fs');
+        const os = require('os');
+        const promisify = require('util').promisify;
+        const readFile = promisify(fs.readFile);
+        const mkdtemp = promisify(fs.mkdtemp);
+        
         const file = path.join(process.cwd(), argv.file);
-        const {
-            stdout
-        } = await execa('js-features-analyser', ['analyse', '--file', file], {
-            preferLocal: true
-        });
-        console.log(await generatePolyfillURL(JSON.parse(stdout)));
+        const fileContents = await readFile(file, 'utf-8');
+        const tempFolder = await mkdtemp(path.join(os.tmpdir(), 'js-features-analyser'));
+        const outputDestination = path.join(tempFolder, 'features.json');
+
+        try {
+            babel.transformSync(fileContents, {
+                plugins: [
+                    [
+                        plugin, {
+                            outputDestination
+                        }
+                    ]
+                ],
+                filename: file,
+                ast: false,
+                code: false
+            });
+            
+            console.log(await generatePolyfillURL(JSON.parse(fs.readFileSync(outputDestination, 'utf-8'))));
+
+        } catch (error) {
+            if (error instanceof SyntaxError) {
+                console.log("Failed to parse the JavaScript from xxx because it has a syntax error.")
+                delete error.stack;
+                delete error.code;
+                delete error.pos;
+                delete error.loc;
+                const result = /: (?<errorType>[\w ]+) \((?<position>\d+:\d+)\)/.exec(error.message);
+                console.log(result.groups.errorType)
+                error.message = error.message.replace(` ${result.groups.errorType} `, '');
+                error.message = error.message.replace(`(${result.groups.position})`, result.groups.position);
+                console.error(error.message);
+            }
+        }
     })
     .help()
     .strict()
